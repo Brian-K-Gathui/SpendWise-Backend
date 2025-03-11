67e0c93d0c00b135268fc8eab8e365e4f8a96a93
Lewis Odero <lewisodero27@gmail.com>
2025-03-11 04:51:40 +0300

    migration

---
+++ migrations/versions/898998b16151_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 898998b16151
+Revises: 13441c65e201
+Create Date: 2025-03-10 13:19:33.701266
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '898998b16151'
+down_revision = '13441c65e201'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
